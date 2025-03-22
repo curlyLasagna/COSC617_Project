293abcf4095b278dff801a1ff39117c857d278a1
Luis Dale <luis.gcodes@gmail.com>
2025-03-21 22:06:43 -0400

    colours

--- components/sticky-footer.tsx
+++ components/sticky-footer.tsx
@@ -5,10 +5,10 @@ export function StickyFooter() {
     <div className="sticky bottom-0 bg-[#7c5cff] max-w-none py-2 z-50">
       <div className="flex justify-center">
         <div className="flex flex-col items-center gap-y-2">
-          Join over 100 million people using Fumblr to fumble the huzz
+          <div>Join over <b>100 million</b> people using Fumblr to fumble the <b>huzz</b></div>
           <div className="flex flex-row gap-4 w-full justify-center">
-            <Button className="w-full rounded-full bg-black text-white">Sign Up</Button>
-            <Button className="w-full rounded-full bg-[#6f53e5] text-white">Log in</Button>
+            <Button className="w-full rounded-full bg-black text-white hover:bg-black/90">Sign Up</Button>
+            <Button className="w-full rounded-full bg-[#6f53e5] hover:bg-[#6048c9]/90 text-white">Log in</Button>
           </div>
         </div>
       </div>
