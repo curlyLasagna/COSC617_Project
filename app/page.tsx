eeb424ada542e05066949f7c3e6c190d457231b0
Latchenvan <tobeejike22@gmail.com>
2025-03-26 01:44:53 -0400

    Added Right Sidebar to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+import RightSidebar from "@/components/ui/RightSidebar";
+
 export default async function Home() {
   return (
-    <>
+    <div className="flex">
+      {/* Main Content */}
       <main className="flex-1 flex flex-col gap-6 px-4">
+        <h1 className="text-2xl font-bold">Welcome to Fumblr</h1>
       </main>
-    </>
+
+      {/* Right Sidebar */}
+      <RightSidebar />
+    </div>
   );
 }
