de8b373e4cacbe28c16874fdea92f996001e41cd
Luis Dale <luis.gcodes@gmail.com>
2025-05-05 18:46:33 -0400

    new users with default avatar

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,4 @@
+import { generate_avatar } from "@/lib/avatar-generate";
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
@@ -9,10 +10,39 @@ export async function GET(request: Request) {
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
-
+  const sb = await createClient();
   if (code) {
-    const supabase = await createClient();
-    await supabase.auth.exchangeCodeForSession(code);
+    const { data: sessionData, error: sessionError } =
+      await sb.auth.exchangeCodeForSession(code);
+    const user = sessionData.user;
+    const { data: userData, error: userError } = await sb
+      .from("users")
+      .select("profile_picture_url, auth_user_id")
+      .eq("auth_user_id", user?.id)
+      .single();
+
+    // If user doesn't have a default avatar
+    if (!userData?.profile_picture_url) {
+      console.log("User doesn't have an avatar");
+      const avatarSVG = generate_avatar();
+      const avatarFileName = `${userData?.auth_user_id}.svg`;
+      const { data: uploadData, error: uploadError } = await sb.storage
+        .from("pfp")
+        .upload(avatarFileName, avatarSVG, {
+          contentType: "image/svg+xml",
+          upsert: true,
+        });
+
+      if (uploadError) {
+        console.error("RIP", uploadError);
+      } else {
+        const avatarURL = sb.storage.from("pfp").getPublicUrl(avatarFileName);
+        await sb
+          .from("users")
+          .update({ profile_picture_url: avatarURL })
+          .eq("auth_user_id", user?.id);
+      }
+    }
   }
 
   if (redirectTo) {
