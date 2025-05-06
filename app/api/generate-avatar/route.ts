5987282a5c7e504cb3b5ae74b3ba3d978d42a886
Luis Dale <luis.gcodes@gmail.com>
2025-05-06 10:59:28 -0400

    that's not supposed to be there

--- app/api/generate-avatar/route.ts
+++
@@ -1,7 +0,0 @@
-import { generate_avatar } from "@/lib/avatar-generate";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const avatarSvg = generate_avatar();
-  return NextResponse.json({ avatar: avatarSvg });
-}
