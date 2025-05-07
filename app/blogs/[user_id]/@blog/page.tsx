f05f900b5dfd7d3368ed4573f5033f81bdd39e34
Andrei Kondrashov <kondraschov.andrew1999@gmail.com>
2025-05-07 15:55:16 -0400

    create parallel routes for blog

---
+++ app/blogs/[user_id]/@blog/page.tsx
@@ -0,0 +1,35 @@
+import { BlogComponent } from "@/components/blog/blog";
+import { fetchBlogAction } from "@/utils/blogs/fetch-blog-action";
+import { fecthUser } from "@/utils/users/fetch-user-action";
+
+export default async function BlogPage({
+  params,
+}: {
+  params: { user_id: string };
+}) {
+  const { user_id } = await params;
+  const { success, error, user } = await fecthUser(user_id);
+
+  if (!success || !user) {
+    console.error("Error fetching user:", error);
+    return <div>Error loading user data</div>;
+  }
+
+  if (error) {
+    console.error("Error fetching user:", error);
+    return <div>Error loading user data</div>;
+  }
+
+  const {
+    success: blog_success,
+    error: blog_err,
+    blog,
+  } = await fetchBlogAction(user_id);
+
+  if (!blog_success || !blog) {
+    console.error("Error fetching blog:", blog_err);
+    return <div>Error loading blog data</div>;
+  }
+
+  return <BlogComponent blog={blog} user={user} />;
+}
